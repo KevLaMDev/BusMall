581ba8ba6a5a1a50938b449fa481f60c3e37b390
KEVIN LAMARCA <LAMARCA.K@GMAIL.COM>
2021-12-17 01:00:38 -0500

    got # times shown data on chart with 2nd data set

--- js/app.js
+++ js/app.js
@@ -45,6 +45,22 @@ const chartData = {
         'rgba(255, 159, 64, 1)'
       ],
       borderWidth: 1
+    },
+    {
+      label: '# of Times Shown',
+      data: [],
+      backgroundColor: [
+        'slategrey'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
     }]
   },
   options: {
@@ -84,12 +100,15 @@ function buttonEventHandler(event) {
   } else {
     let productNames = [];
     let votes = [];
+    let views = [];
     for (let i = 0; i < allProducts.length; i++) {
       productNames.push(allProducts[i].name);
       votes.push(allProducts[i].selected);
+      views.push(allProducts[i].views);
     }
     chartData.data.labels = productNames;
     chartData.data.datasets[0].data = votes;
+    chartData.data.datasets[1].data = views;
     new Chart(ctx, chartData);
     button.removeEventListener('click', buttonEventHandler);
     let stringifiedProducts = JSON.stringify(allProducts);
